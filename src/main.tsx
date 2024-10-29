f3330b0620eee716edf86264713bb577f279d69f
dexterdc <sebasdc9@hotmail.com>
2024-10-29 17:39:26 -0600

    add path to router

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import Layout from './Layout'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Layout from "./Layout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CoursesForm from "./components/forms/CoursesForm";
+import EmployeeForm from "./components/forms/EmployeeForm";
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <App />,
+      },
+      {
+        path: "/courses",
+        element: <CoursesForm />,
+      },
+      {
+        path: "/employees",
+        element: <EmployeeForm />,
       },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);

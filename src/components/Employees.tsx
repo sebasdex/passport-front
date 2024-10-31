06b9c83908d82e0250b3a7e0b44d8010d463f9d5
DexterDC <sebasdc9@hotmail.com>
2024-10-31 11:13:59 -0600

    create Employees component

---
+++ src/components/Employees.tsx
@@ -0,0 +1,14 @@
+import TableEmployees from "./dataTable/TableEmployees"
+import EmployeeForm from "./forms/EmployeeForm"
+
+
+function Employees() {
+    return (
+        <>
+            <EmployeeForm />
+            <TableEmployees />
+        </>
+    )
+}
+
+export default Employees

ee58a5f8efe61ea63af51a7132a1d023f99c1978
DexterDC <sebasdc9@hotmail.com>
2024-11-09 13:45:34 -0600

    add table users component

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,11 +1,13 @@
+import TableUsers from "./dataTable/TableUsers"
 import UsersForm from "./forms/UsersForm"
 
 
 function Users() {
     return (
-        <>
+        <div className="flex items-start gap-4">
             <UsersForm />
-        </>
+            <TableUsers />
+        </div>
     )
 }
 

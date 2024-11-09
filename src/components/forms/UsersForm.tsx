f4217c8f36b5c4eca7a3c963eeed6608749c04fc
DexterDC <sebasdc9@hotmail.com>
2024-11-09 13:46:31 -0600

    update fetch to update user data

--- src/components/forms/UsersForm.tsx
+++ src/components/forms/UsersForm.tsx
@@ -2,6 +2,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import ResponsiveDialog from "../DialogAlert";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 interface UserFormProps {
     email: string;
@@ -19,8 +21,11 @@ interface Employee {
     lastName: string;
 }
 function UsersForm() {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<UserFormProps>();
+    const { register, handleSubmit, formState: { errors }, reset, setValue } = useForm<UserFormProps>();
     const [employees, setEmployees] = useState<Employee[]>([]);
+    const [checkUpdate, setCheckUpdate] = useState<boolean>(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
     const showAlert = (text: string) => {
         toast.success(text, {
             position: "top-right",
@@ -51,18 +56,30 @@ function UsersForm() {
             return;
         }
         try {
-            const response = await fetch("http://localhost:3000/api/addUser", {
-                method: "POST",
+            const response = id ? await fetch(`http://localhost:3000/api/updateUser/${id}`, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(data)
+            }) : await fetch("http://localhost:3000/api/addUser", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
             });
-            if (response.ok) {
+            if (response.ok && id) {
                 const successData = await response.json();
                 showAlert(successData.message);
-                reset();
-            } else {
+                handleNewUser();
+            }
+            else if (response.ok && !id) {
+                const successData = await response.json();
+                showAlert(successData.message);
+                handleNewUser();
+            }
+            else {
                 const errorData = await response.json();
                 showError(errorData.message);
             }
@@ -82,11 +99,42 @@ function UsersForm() {
             console.error(error);
         }
     }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (id) {
+                try {
+                    const response = await fetch(`http://localhost:3000/api/getUser/${id}`);
+                    if (response.ok) {
+                        const data = await response.json()
+                        setValue("email", data.user.email);
+                        setValue("role", data.user.role);
+                        setValue("employeeId", data.user.employeeId);
+                    } else {
+                        showError("Error al cargar los datos del usuario")
+                    }
+                } catch (error) {
+                    console.error("Error al cargar los datos del usuario", error);
+                }
+            }
+        }
+        fetchData();
+    }, [id, setValue]);
+
+    const handleNewUser = () => {
+        navigate("/users");
+        reset();
+        setCheckUpdate(false);
+    };
     return (
         <section className="flex flex-col gap-4 p-4">
-            <h1 className="text-3xl font-bold">
-                Formulario de usuarios
-            </h1>
+            <h1 className="text-4xl font-bold">Formulario de usuarios</h1>
+            <button
+                className="text-blue-900 font-semibold bg-blue-200 p-2 rounded-md hover:bg-blue-300 transition-all duration-300"
+                onClick={() => handleNewUser()}
+            >
+                Nuevo registro
+            </button>
             <form className="flex flex-col gap-4">
                 <label htmlFor="email">Email</label>
                 <input type="email" {...register("email", { required: true })} placeholder="ej. ivan@gmail.com" className="border-2 border-blue-200 p-2 rounded-md" />
@@ -108,19 +156,53 @@ function UsersForm() {
                     ))}
                 </select>
                 {errors.employeeId && <p role="alert" className="text-red-500 -mt-2 font-semibold text-sm">Empleado es requerido</p>}
-                <label htmlFor="password">Contraseña</label>
-                <input type="password" id="password" {...register("password", { required: true })} placeholder="ej. 123456789" className="border-2 border-blue-200 p-2 rounded-md" />
-                {errors.password && <p role="alert" className="text-red-500 -mt-2 font-semibold text-sm">Contraseña es requerido</p>}
-                <label htmlFor="confirmPassword">Confirmar contraseña</label>
-                <input type="password" {...register("confirmPassword", { required: true })} placeholder="ej. 123456789" className="border-2 border-blue-200 p-2 rounded-md" />
-                {errors.confirmPassword && <p role="alert" className="text-red-500 -mt-2 font-semibold text-sm">Contraseña es requerido</p>}
+                {id && <div className="flex justify-between p-2">
+                    <label htmlFor="chooseUpdate">¿Modificar contraseña?</label>
+                    <input
+                        type="checkbox"
+                        className="w-5 border-2 border-blue-200 p-2 rounded-md"
+                        checked={checkUpdate}
+                        onChange={(e) => setCheckUpdate(e.target.checked)}
+                    />
+                </div>}
+                {(!id || checkUpdate) && (
+                    <>
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            {...register("password", { required: true })}
+                            placeholder="ej. 123456789"
+                            className="border-2 border-blue-200 p-2 rounded-md"
+                        />
+                        {errors.password && (
+                            <p role="alert" className="text-red-500 -mt-2 font-semibold text-sm">
+                                Contraseña es requerido
+                            </p>
+                        )}
+
+                        <label htmlFor="confirmPassword">Confirmar contraseña</label>
+                        <input
+                            type="password"
+                            {...register("confirmPassword", { required: true })}
+                            placeholder="ej. 123456789"
+                            className="border-2 border-blue-200 p-2 rounded-md"
+                        />
+                        {errors.confirmPassword && (
+                            <p role="alert" className="text-red-500 -mt-2 font-semibold text-sm">
+                                Contraseña es requerido
+                            </p>
+                        )}
+                    </>
+                )}
+
+
                 <ResponsiveDialog
-                    iconButton="Registrar"
+                    iconButton={id ? "Actualizar" : "Registrar"}
                     handleConfirm={handleSubmit(onSubmit)}
-                    buttonText="Aceptar"
+                    buttonText={id ? "Actualizar" : "Aceptar"}
                     className="text-white font-semibold bg-blue-900 p-2 rounded-md hover:bg-blue-800 transition-all duration-300 mt-6"
-                    dialogText="Los datos se guardarán en la base de datos tal y como lo ingresaste"
-                    dialogQuestion="¿Estás seguro de que deseas agregar este usuario?"
+                    dialogText={id ? "Los datos actualizados se guardarán en la base de datos tal y como lo ingresaste" : "Los datos se guardarán en la base de datos tal y como lo ingresaste"}
+                    dialogQuestion={id ? "¿Estás seguro de que deseas actualizar este registro?" : "¿Estás seguro de que deseas registrar este formulario?"}
                 />
             </form>
         </section>

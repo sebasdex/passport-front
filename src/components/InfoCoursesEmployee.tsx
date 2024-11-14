7421c81a0a164f01b2581707fc403683a488663e
DexterDC <sebasdc9@hotmail.com>
2024-11-13 20:01:59 -0600

    update icons

--- src/components/InfoCoursesEmployee.tsx
+++ src/components/InfoCoursesEmployee.tsx
@@ -1,3 +1,5 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import BlockIcon from '@mui/icons-material/Block';
 interface Employee {
   id: number;
   employeeNumber: string;
@@ -23,22 +25,6 @@ type InfoCoursesEmployeeProps = {
   dataCourse: Courses[];
 };
 function InfoCoursesEmployee({ dataCourse }: InfoCoursesEmployeeProps) {
-  const checkApproved = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.9"
-      stroke="currentColor"
-      className="size-9"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
-      />
-    </svg>
-  );
   return (
     <>
       {dataCourse.map((course) => (
@@ -63,18 +49,19 @@ function InfoCoursesEmployee({ dataCourse }: InfoCoursesEmployeeProps) {
               <span className="font-normal">{course.instructor}</span>
             </p>
             <p className={`text-green-600 font-bold ${course.endDate ? "" : "text-red-500"}`}>
-              <span className="text-blue-900">Completado el:</span> {course.endDate ? new Date(course.endDate).toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' }) : "No finalizado"}
+              <span className="text-blue-900">Fecha de finalización:</span>{" "}
+              {course.endDate ? new Date(course.endDate).toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' }) : "Sin fecha definida"}
             </p>
           </article>
           <footer className="flex flex-col gap-2 items-center lg:flex-row lg:justify-between lg:items-end">
             <p className="text-blue-800 italic">
               Fecha del curso: {new Date(course.startDate).toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' })} - {course.endDate ? new Date(course.endDate).toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' }) : "Sin fecha de finalización"}
             </p>
-            <div className={`h-32 w-32 rotate-12 rounded-full  flex flex-col items-center justify-center ${course.approved ? "bg-green-300 text-green-600" : "bg-red-500 text-white"}`}>
-              {checkApproved}
-              <p className="text-lg font-bold uppercase flex flex-col items-center">
-                {course.approved ? "Aprobado" : "Pendiente"}
-                <span className="block text-sm font-normal">{course.endDate ? new Date(course.endDate).toLocaleDateString('es-ES') : ""}</span>
+            <div className={`h-32 w-32 rotate-12 rounded-full flex flex-col items-center justify-center ${course.approved ? "bg-green-300 text-green-600" : "bg-red-500 text-white"}`}>
+              {course.approved ? <CheckCircleIcon /> : <BlockIcon fontSize='large' />}
+              <p className="text-lg font-bold uppercase flex flex-col items-center text-center">
+                {course.approved ? "Aprobado" : "Denegado"}
+                <span className="block text-xs font-normal">{course.endDate ? new Date(course.endDate).toLocaleDateString('es-ES') : "Por definir"}</span>
               </p>
             </div>
           </footer>
